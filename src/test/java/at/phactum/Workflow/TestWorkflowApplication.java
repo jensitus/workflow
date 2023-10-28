7eba0dc31cf06a5dd3f56d91733ee4b6017a7c5f
Jens Kornacker <jens.kornacker@phactum.at>
2023-10-28 21:14:31 +0200

    initial commit

---
+++ src/test/java/at/phactum/Workflow/TestWorkflowApplication.java
@@ -0,0 +1,23 @@
+package at.phactum.Workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestWorkflowApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(WorkflowApplication::main).with(TestWorkflowApplication.class).run(args);
+	}
+
+}
